359dda8937c4bc76f7329716ddd9d17c7be46213
Micky Woo <mwoo@seattleu.edu>
2020-01-24 10:27:14 -0800

    part 1 fizzbuz

--- 1-fizzbuzz/index.js
+++ 1-fizzbuzz/index.js
@@ -1,8 +1,9 @@
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description: program that counts from 1, outputting _fizz if divisible by 3, 
+_buzz if divisible by 5, and _fizzbuzz if divisible by both.
+Input:an integer
+Output:numbers and strings of fizz, buzz and fizzbuzz if divisible by 3,5,or both
+Usage: 1-fizzbuzz integer
 */
 
 // get input using getargs
@@ -11,20 +12,20 @@ let input = getargs.getIntegerArg()
 
 // test that input is a number and exit with error
 // TODO check if input is not an integer
-if ( ) {
-  // TODO provide usage statement
+if ( isNaN (input) || !Number.isInteger(input)) {
+  console.log(`node 1-fizzbuz please input integer`)
 }
 else {
   // iterate from 1 to the value of input
   // TODO provide info to for loop to iterate from 1 to value of input
-  for ( ) {
+  for ( let i = +1; i<=input; i++ ) {
     
     //TODO provide for tests for divisible for 3,5, and 15 to create desired output
-    if ( ) {
+    if ( i % 15 == 0 ) {
       console.log(`${i} fizzbuzz`);
-    } else if ( ) {
+    } else if ( i % 3 == 0 ) {
       console.log(`${i} fizz`);
-    } else if ( ) {
+    } else if ( i % 5 == 0 ) {
       console.log(`${i} buzz`);
     } else {
       console.log(`${i}`);
