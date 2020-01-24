f56d156e487edff19e348ddc59b1d4cb9365176c
Micky Woo <mwoo@seattleu.edu>
2020-01-24 12:00:51 -0800

    node 4

--- 4-reverse-string-iterate/index.js
+++ 4-reverse-string-iterate/index.js
@@ -1,9 +1,9 @@
 //TODO fill in comment template
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description:identifies that a string has been inputted and returns the reversed string by iterating through the full length of the string
+Input: string
+Output:reverse string character by character
+Usage: 4-rever-string-iterate
 */
 
 // get input using getargs
@@ -13,8 +13,9 @@ let input = getargs.getStringArg()
 function reverseWithFullIteration(str) {
     let result = '';
     // TODO loop from end of str (str.length-1) to beginning
-    for ( ) {
+    for ( let i = str.length-1; i>=0; i-- ) {
         // TODO add the letter in the current position to the result string.
+        result += str[i];
     }
     return result;
 }
@@ -25,8 +26,8 @@ function reverseWithHalfIteration(str) {
 
 //test that it is a string
 //TODO check for string argument and if no string is entered provide a usage statement and quit
-if ( ) {
-    // TODO provide a usage statement
+if ( !input  ) {
+    console.log(`node 4-reverse-string-iterate please input string`)
 }
 else {
     // use functions to reverse string
