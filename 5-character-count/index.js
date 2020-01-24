ba4348e9e806986064e714b0bc206a50f41710b3
Micky Woo <mwoo@seattleu.edu>
2020-01-24 13:39:24 -0800

    pt 5

--- 5-character-count/index.js
+++ 5-character-count/index.js
@@ -52,19 +52,6 @@ for (let row of charArr){
     console.log(` ${charArr[i].char}: ${charArr[i].count}`);
   }
 }
-  // TODO initialize charCount object to count characters
-  // TODO turn the input into an array called characters using the split method
-  
-  // TODO iterate through array to create object that has character for key and count for value
-  
-  // TODO initialize a new array called charArr
-  // TODO loop through every character in charCount 
 
-
-
-  // TODO use the sort method of charArr with a custom compare function to sort the array by the count values (least to greatest)
-
-  // TODO output the content of the array to the console, showing each character and the count
-  
 }
 
